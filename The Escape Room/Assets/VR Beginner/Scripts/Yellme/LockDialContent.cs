39f4e24703393a06346a6ad2d986f1acc791c4a8
118dg <118dg@naver.com>
2021-03-12 13:25:50 +0900

    자물쇠랑 보물상자 그냥 애니메이션 처리 ㅠ

--- The Escape Room/Assets/VR Beginner/Scripts/Yellme/LockDialContent.cs
+++ The Escape Room/Assets/VR Beginner/Scripts/Yellme/LockDialContent.cs
@@ -7,6 +7,29 @@ public class LockDialContent : MonoBehaviour
 
     int m_DialNumber = 0;
 
+    [Header("Effects")]
+    public Animator CauldronAnimator;
+
+    bool m_CanBrew = false;
+
+    [Header("Audio")]
+    public AudioSource AmbientSoundSource;
+
+    float m_StartingVolume;
+
+    private void Start()
+    {
+        m_StartingVolume = AmbientSoundSource.volume;
+        AmbientSoundSource.volume = m_StartingVolume * 0.2f;
+    }
+
+    public void Open()
+    {
+        CauldronAnimator.SetTrigger("Open");
+        m_CanBrew = true;
+        AmbientSoundSource.volume = m_StartingVolume;
+    }
+
     public void ChangeDialNumber(int step)
     {
         m_DialNumber = step;
---
+++ The Escape Room/Assets/VR Beginner/Scripts/Yellme/UnlockDialReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Will Trigger the OnUnlockCollision when a gameObject with a tag "unlockdialkey" collide with the collider on which that script is
+/// </summary>
+public class UnlockDialReceiver : MonoBehaviour
+{
+    public UnityEvent OnUnlockCollision;
+    //public bool DestroyedOnTriggered;
+
+    void OnCollisionEnter(Collision other)
+    {
+        //var proj = other.rigidbody.GetComponent<MagicBallProjectile>();
+
+        //if (proj != null)
+        if(other.gameObject.tag == "unlockdialkey")
+        {
+            //Destroy(proj);
+            OnUnlockCollision.Invoke();
+            //if (DestroyedOnTriggered)
+                //Destroy(this);
+        }
+    }
+}
